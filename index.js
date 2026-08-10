ed731139dac2fce17822ef1f34bbc897ff33d178
adrianquintero09 <adrianquinteropinzon@gmail.com>
2026-08-10 09:22:55 -0500

    Update Files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 
 const app = express();
 
-app.use(express.json()); //middlewares
+app.use(express.json()); 
 
 app.listen(process.env.PORT, () => {
     console.log(`server http://localhost:${process.env.PORT}`);
